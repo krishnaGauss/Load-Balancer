9b9f55aa0d9295265a00042d646a270d56dc0880
krishnaGauss <goswamikrishna549@gmail.com>
2025-05-19 00:53:50 +0530

    func added

--- serverpool/server_pool.go
+++ serverpool/server_pool.go
@@ -1,27 +1,44 @@
 package serverpool
 
-import(
+import (
 	"fmt"
-	"sync"
 	"github.com/krishnaGauss/load-balancer/backend"
+	"sync"
 )
 
-
-type ServerPool interface{
+type ServerPool interface {
 	GetBackends() []backend.Backend
 	GetNextValidPeer() backend.Backend
 	addBackend(backend.Backend)
 	GetServerPoolSize() int
 }
 
-type roundRobinServerPool struct{
+type roundRobinServerPool struct {
 	backends []backend.Backend
-	mux *sync.RWMutex
-	current int
+	mux      *sync.RWMutex
+	current  int
 }
 
-func (s *roundRobinServerPool) Rotate(backend.Backend){
+func (s *roundRobinServerPool) Rotate() backend.Backend {
 	s.mux.Lock()
-	s.current = (s.current+1)
-	
+	s.current = (s.current + 1) % GetServerPoolSize()
+	s.mux.Unlock()
+	return s.backends[s.current]
 }
+
+func (s *roundRobinServerPool) GetNextValidPeer() backend.Backend {
+	for i := 0; i < GetServerPoolSize(); i++ {
+		nextPeer := s.Rotate()
+		if nextPeer.IsAlive() {
+			return nextPeer
+		}
+	}
+	return nil
+
+}
+
+func (s *roundRobinServerPool)GetServerPoolSize() int{
+	return len(s.backends)
+}
+
+
